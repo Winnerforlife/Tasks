096ecd08a1004b076220001a411f8f4f32e5266a
Winnerforlife <54172899+Winnerforlife@users.noreply.github.com>
2020-03-11 13:51:38 +0200

    first task

---
+++ Solution.py
@@ -0,0 +1,10 @@
+###########################################
+
+def summation(number):
+    answer = 0
+    while number!=0:
+        answer += number
+        number -= 1
+    return answer
+
+###########################################
